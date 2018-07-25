c688dc1b2269533ffcf41038b46cba54c34327f5
Guy Moore <guy.moore.normal@gmail.com>
2018-07-25 03:34:45 -0700

    Remove extra equals in auth-wrapper.sh

--- auth-wrapper.sh
+++ auth-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR=="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 "$SCRIPT_DIR"/env/bin/python "$SCRIPT_DIR"/authorized-keys-iam.py auth "$1"
